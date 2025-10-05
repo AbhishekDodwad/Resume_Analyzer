32e4e8eeba577fce0cf7d4cd3cda3760d2517d93
Abhishek dodwad <you@example.com>
2025-10-06 01:04:45 +0530

    added env

--- ResumeAnalyzer/src/main/java/com/Project/ResumeAnalyzer/Config/WebConfig.java
+++ ResumeAnalyzer/src/main/java/com/Project/ResumeAnalyzer/Config/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig {
           // @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(  "http://localhost:5173",
+                                "http://localhost:3000",
+                                "https://resume-analyzer-ckk4zlp9f-abhishek-dodwads-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
